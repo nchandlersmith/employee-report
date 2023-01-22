6e67cde35d752a6097c28cfc2a85fa927bfe5013
Nathan Chandler-Smith <nate.chandlersmith@gmail.com>
2023-01-21 20:23:56 -0600

    Finish kata

---
+++ src/EmployeeReport.spec.ts
@@ -0,0 +1,42 @@
+import {employee, report} from "./EmployeeReport";
+
+
+
+const employees: employee[] = [
+  {
+    age: 17,
+    name: "Max"
+  },
+  {
+    age: 18,
+    name: "Sepp"
+  },
+  {
+    age: 15,
+    name: "Nina"
+  },
+  {
+    age: 51,
+    name: "Mike"
+  }
+]
+
+describe('EmployeeReport', () => {
+  it('should return only employees over the age of 18', () => {
+    const result = report(employees)
+    expect(result[0].age).toBeGreaterThanOrEqual(18)
+    expect(result[1].age).toBeGreaterThanOrEqual(18)
+    expect(result.length).toEqual(2)
+  });
+  it('should sort employees by their name', () => {
+    const result = report(employees)
+    expect(result.length).toEqual(2)
+    expect(result[0].name.toLowerCase()).toEqual("sepp")
+    expect(result[1].name.toLowerCase()).toEqual("mike")
+  });
+  it('should return names capitalized', () => {
+    const result = report(employees)
+    expect(result[0].name).toEqual("SEPP")
+    expect(result[1].name).toEqual("MIKE")
+  })
+})
---
+++ src/EmployeeReport.ts
@@ -0,0 +1,20 @@
+export interface employee {
+  name: string
+  age: number
+}
+
+export function report(employees: employee[]): employee[] {
+  const filtered = employees.filter(employee => employee.age >= 18)
+  const sorted = filtered.sort((a,b) => sortByName(a,b))
+  return sorted.map(employee => ({name: employee.name.toUpperCase(), age: employee.age}))
+}
+
+function sortByName(a: employee, b: employee): number {
+  if (a.name > b.name) {
+    return -1
+  }
+  if (a.name < b.name) {
+    return 1
+  }
+  return 0
+}
